51bfb033c73bb108ed8ea88ba7f1b46ac49ef4a5
Mikael Sundberg <mikael.sundberg@volvocars.com>
2022-06-13 14:19:14 +0200

    update versions

--- src/main/java/se/cleancode/CqrsexampleApplication.java
+++ src/main/java/se/cleancode/CqrsexampleApplication.java
@@ -1,13 +1,11 @@
 package se.cleancode;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableAutoConfiguration
 public class CqrsexampleApplication {
 
 	public static void main(String[] args) {
